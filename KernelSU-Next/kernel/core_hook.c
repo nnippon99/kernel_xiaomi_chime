8eea01bbd93ee978e239561aa3fe62411b80ac5b
nnippon99 <adamayyad1999@hotmail.com>
2025-03-20 06:55:02 -0500

    chime: Update KernelSU-Next
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>

--- KernelSU-Next/kernel/core_hook.c
+++ KernelSU-Next/kernel/core_hook.c
@@ -494,6 +494,9 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 		bool enabled = (arg3 != 0);
 		if (enabled == ksu_su_compat_enabled) {
 			pr_info("cmd enable su but no need to change.\n");
+			if (copy_to_user(result, &reply_ok, sizeof(reply_ok))) {// return the reply_ok directly
+				pr_err("prctl reply error, cmd: %lu\n", arg2);
+			}
 			return 0;
 		}
 
@@ -647,26 +650,6 @@ int ksu_handle_setuid(struct cred *new, const struct cred *old)
 	// try umount lsposed dex2oat bins
 	try_umount("/apex/com.android.art/bin/dex2oat64", false, MNT_DETACH);
 	try_umount("/apex/com.android.art/bin/dex2oat32", false, MNT_DETACH);
-
-	// try umount pixelify gphotos spoof configs
-	try_umount("/system/etc/sysconfig/pixel_2017_exclusive.xml", false, MNT_DETACH);
-	try_umount("/system/etc/sysconfig/pixel_2018_exclusive.xml", false, MNT_DETACH);
-	try_umount("/system/etc/sysconfig/pixel_2019_exclusive.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_2016_exclusive.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_2017_exclusive.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_2018_exclusive.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_2019_exclusive.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_experience_2017.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_experience_2018.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_experience_2019.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_experience_2019_midyear.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_experience_2020.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_experience_2020_midyear.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_experience_2021.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_experience_2021_midyear.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_experience_2022.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixel_experience_2022_midyear.xml", false, MNT_DETACH);
-	try_umount("/product/etc/sysconfig/pixelify_experience.xml", false, MNT_DETACH);
 
 	return 0;
 }
